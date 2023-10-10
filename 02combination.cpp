a5f6a0fe2fda10ac5e89d104c5fd4b9d3786c092
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-10 20:48:48 +0530

    02combination.cpp

---
+++ 02combination.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the no:";
+    cin>>a>>b;
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a/b;
+    int g=a%b;
+    cout<<"Addition of given no is:"<<c<<endl;
+     cout<<"Subtraction of given no is:"<<d<<endl;
+      cout<<"Multiply of given no is:"<<e<<endl;
+       cout<<"divided of given no is:"<<f<<endl;
+        cout<<"Modulus of given no is:"<<g<<endl;
+}
