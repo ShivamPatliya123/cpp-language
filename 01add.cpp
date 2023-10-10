1fe7bcc39aee90b32b3abb04358cdee7548f6d2f
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-10 20:48:01 +0530

    01add.cpp

---
+++ 01add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cout<<"Enter the no x and y:";
+    cin>>x>>y;
+    z=x+y;
+    cout<<"Addition of given no:"<<z;
+    return 0;
+}
