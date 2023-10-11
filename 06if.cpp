920fb0ec37ad9083338f0ec96db0ba5fb5fbbd7f
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-11 20:42:59 +0530

    06if.cpp

---
+++ 06if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the no:";
+    cin>>a;
+    b=a%2;
+    if(b==0)
+     cout<<"NO is even"<<endl;
+     else
+     cout<<"no is odd";
+}
