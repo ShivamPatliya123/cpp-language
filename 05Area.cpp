b6150e6a215687479972e69db71a5ba17cf3a3f2
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-11 20:42:14 +0530

    05Area.cpp

---
+++ 05Area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float base,height,area;
+    cout<<"Enter the base:";
+    cin>>base;
+    cout<<"Enter the height";
+    cin>>height;
+    area=.5*base*height;
+    cout<<"Area of Tringle:"<<area;
+    return 0;
+}
