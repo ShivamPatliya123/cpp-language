b3ce21a290283f1d4c663fe7bcc3058fd27f786c
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-11 20:44:00 +0530

    07max.cpp

---
+++ 07max.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the no a and b:";
+    cin>>a>>b;
+    if(a>b)
+    cout<<"A is maximum";
+    else if(a<b)
+    cout<<"B is maximum";
+    else
+    cout<<"A and B are equal";
+return 0;
+}
