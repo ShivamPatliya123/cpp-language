b9f266224f6efd31544a6f0f4ed06c49426b5bc3
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-14 20:56:02 +0530

    13constructer.cpp

---
+++ 13constructer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class tast
+{
+    public:
+    tast()
+    {
+        cout<<"This is constructer:";
+    }
+};
+int main()
+{
+    tast t;
+    return 0;
+}
